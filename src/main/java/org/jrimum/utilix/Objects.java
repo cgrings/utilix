cac490d7f060175e53dd3a364715cb1139ed2d5b
Claiton Grings <claiton.grings@gmail.com>
2016-09-28 18:43:52 -0300

    Atualização Java 7 e dependências

--- src/main/java/org/jrimum/utilix/Objects.java
+++ src/main/java/org/jrimum/utilix/Objects.java
@@ -29,7 +29,7 @@
 
 package org.jrimum.utilix;
 
-import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 
 /**
--- src/test/java/org/jrimum/utilix/text/TestStrings.java
+++ src/test/java/org/jrimum/utilix/text/TestStrings.java
@@ -29,7 +29,7 @@
 
 package org.jrimum.utilix.text;
 
-import static org.apache.commons.lang.StringUtils.EMPTY;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
